27c2d88a1f028c79645648e8e5c7a5e8ea7d6e82
Makai Mann <makaim@cs.stanford.edu>
2022-08-04 09:51:21 -0400

    Make pybullet render type configurable

--- rand_action.py
+++ rand_action.py
@@ -1,11 +1,13 @@
 #!/usr/bin/env python3
 
 import gym
+import pybullet as p
 import simple_driving
 import time
 
 if __name__ == '__main__':
-    env = gym.make('SimpleDriving-v0')
+    # switch to p.DIRECT here for RGB camera only
+    env = gym.make('SimpleDriving-v0', pb_connect=p.GUI)
     obs = env.reset()
     for _ in range(1000):
         obs, reward, done, info = env.step(env.action_space.sample())
