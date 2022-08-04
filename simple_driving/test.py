54d380fc60f8d2641bc65ad2e9d46d9862f18d26
mingyucai <41490465+mingyucai@users.noreply.github.com>
2022-08-04 01:26:47 -0400

    Update test.py
    Right now, it can record the video. But the simulation time is too fast.  It seems we can use the "p.startStateLogging" and "p.STATE_LOGGING_VIDEO_MP4" to debug.

--- simple_driving/test.py
+++ simple_driving/test.py
@@ -1,8 +1,10 @@
+import matplotlib.pyplot as plt
 import pybullet as p
 import time
 import  pybullet_data
-physicsClient = p.connect(p.GUI)
-p.resetDebugVisualizerCamera(cameraDistance=1.0, cameraYaw=-90, cameraPitch=-45, cameraTargetPosition = [-0.1, -0.0, 0.65])
+physicsClient = p.connect(p.GUI, options="width=1920 --height=1080 --mp4=\"test.mp4\" --mp4fps=240")
+# physicsClient = p.connect(p.GUI)
+p.resetDebugVisualizerCamera(cameraDistance=10.0, cameraYaw=-90, cameraPitch=-45, cameraTargetPosition = [-0.1, -0.0, 0.65])
 # p.resetSimulation()
 p.setAdditionalSearchPath(pybullet_data.getDataPath())
 p.setGravity(0, 0, -10)
@@ -18,6 +20,10 @@ GoalID = p.loadURDF("resources/simplegoal.urdf",
 startPos = [0, 0, 0.2]
 startOrientation = p.getQuaternionFromEuler([0, 0, 0])
 carID = p.loadURDF("resources/simplecar.urdf", startPos, startOrientation)
+
+#TO DO: Save Img of camera
+#img = p.getCameraImage(width=1920, height=1080, renderer=p.ER_BULLET_HARDWARE_OPENGL)
+
 for i in range(1000):
     pos, ori = p.getBasePositionAndOrientation(carID)
     p.applyExternalForce(carID, 0, [50, 0, 0], pos, p.WORLD_FRAME)
